ea36be05e7efdd63f6ce8afc1ae57d31c08ce1bc
youssefelwelely578-spec <youssefelwelely578@gmail.com>
2025-09-26 19:24:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,14 @@
 # server.py
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return "Bot is alive!"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    # Bind to Render Free assigned port
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
