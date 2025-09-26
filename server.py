e1b0dc75d9253a450b877845ff13cc328c83549c
youssefelwelely578-spec <youssefelwelely578@gmail.com>
2025-09-26 19:04:23 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,8 @@
+# server.py
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Bot is alive!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
