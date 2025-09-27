e41c7a0311207ae83ebd7ff782710951f01d5855
youssefelwelely578-spec <youssefelwelely578@gmail.com>
2025-09-27 04:29:21 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-# server.py
 from flask import Flask
 import os
 
@@ -6,8 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot is running"
+    return "Telegram AI Personal Trainer is running!"
 
 if __name__ == "__main__":
+    # Render sets PORT in environment variables
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
