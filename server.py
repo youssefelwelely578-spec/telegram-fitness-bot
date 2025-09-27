6975704e33d97010592c4769d0358b24dab4d583
youssefelwelely578-spec <youssefelwelely578@gmail.com>
2025-09-27 03:50:48 +0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Bot is running"
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
